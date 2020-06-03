5fab2509e0d49ca13444686fbb7e6add61872035
DavidWayneAustinPHP <davidaustinwayne88@gmail.com>
2020-06-03 17:59:21 +0100

    Made the game more cute, now carrot invasion and bunny trying to shoot the carrots heheh

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,7 +14,7 @@ def run_game():
 	
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	
-	pygame.display.set_caption("Alien Invasion")
+	pygame.display.set_caption("Carrot Invasion")
 	
 	# Make the Play button.
 	play_button = Button(ai_settings, screen, "Play")
--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
 		self.ai_settings = ai_settings
 	
 		# Load the ship image and get its rect.
-		self.image = pygame.image.load('images/ship.jpg')
+		self.image = pygame.image.load('images/bunny.jpg')
 		self.rect = self.image.get_rect()
 		self.screen_rect = screen.get_rect()
 	
